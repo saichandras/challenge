3084dce10d5449116619902ff9bfaccafd1e9d40
saichandras <saichandrasriram06@gmail.com>
2021-06-17 17:22:44 -0500

    Added Email JS and small edits

--- src/components/contactMe/ContactMe.js
+++ src/components/contactMe/ContactMe.js
@@ -4,6 +4,7 @@ import TextInputs from "../textInputs/TextInputs";
 import TextAreaInput from "../textAreaInput/TextAreaInput";
 import ContactSubmitButton from "../contactSubmitButton/ContactSubmitButton";
 import { toast } from "react-toast";
+import emailjs from 'emailjs-com';
 
 const ContactMe = () => {
     const [email, setEmail] = useState("");
@@ -12,10 +13,31 @@ const ContactMe = () => {
 
     const sendEmail = (event) => {
         event.preventDefault();
+
+        //Can use emailJS or any other email service
+        /*
+        emailjs.sendForm('gmail', 'templateId', event.target, 'userId')
+            .then((result) => {
+                console.log(result.text);
+                toast.success("Email sent successfully!", {
+                    backgroundColor: "green"
+                });
+                setEmail("");
+                setName("");
+                setMessage("");
+            }, (error) => {
+                return toast.error("Email not sent, please try again!", {
+                    backgroundColor: "#F54748"
+                });
+            });
+        }
+        */
+
         try{
             //Email Logic Here
 
-            toast.success("Eamil sent successfully!", {
+
+            toast.success("Email sent successfully!", {
                 backgroundColor: "green"
             });
             setEmail("");
