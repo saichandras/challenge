ff4401c23984e37b27e9c7dd0e936f334c3a8daf
saichandras <saichandrasriram06@gmail.com>
2021-06-17 14:58:16 -0500

    Progress towards Information Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Information from "./components/information/Information";
 import ContactMe from "./components/contactMe/ContactMe";
+import axios from "axios";
 
 const App = () => {
-    return(
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        fetchUserData();
+    }, [])
+
+    const fetchUserData = async () => {
+        try {
+            let response = await axios.get("https://randomuser.me/api");
+            setUserData(response.data.results[0]);
+            console.log(response.data.results[0]);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    if (userData.length === 0) {
+        return (
+            <div className="App">
+                <div className="App-loader-container">
+                    <div className="App-loader"/>
+                </div>
+            </div>
+        );
+    }
+
+    return (
         <div className="App">
-          <div className="App-container">
-            <Information/>
-            <ContactMe/>
-          </div>
+            <div className="App-container">
+                <Information userData={userData}/>
+                <ContactMe/>
+            </div>
         </div>
     );
 };
--- src/components/information/Information.js
+++ src/components/information/Information.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import './Information.css';
+import {faAddressBook, faMap, faEnvelope, faPhone, faBirthdayCake, faKey} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const Information = () => {
+
+const Information = ({userData}) => {
+
+    const styles = {
+        fontAwesomeIcon: {
+            color: '#60a9ff'
+        }
+    }
+
     return (
         <div className="Information">
             <div className="Information-image-icons-container">
                 <div className="Information-image-container">
-
+                    <img className="Information-image"
+                        src={userData.picture.large}
+                         alt="Photo"/>
                 </div>
                 <div className="Information-icons-container">
-
+                    <div className="Information-icons-container-row">
+                        <FontAwesomeIcon style={styles.fontAwesomeIcon} icon={faAddressBook}/>
+                        <FontAwesomeIcon style={styles.fontAwesomeIcon} icon={faMap}/>
+                    </div>
+                    <div className="Information-icons-container-row">
+                        <FontAwesomeIcon style={styles.fontAwesomeIcon} icon={faEnvelope}/>
+                        <FontAwesomeIcon style={styles.fontAwesomeIcon} icon={faPhone}/>
+                    </div>
+                    <div className="Information-icons-container-row">
+                        <FontAwesomeIcon style={styles.fontAwesomeIcon} icon={faBirthdayCake}/>
+                        <FontAwesomeIcon style={styles.fontAwesomeIcon} icon={faKey}/>
+                    </div>
                 </div>
             </div>
             <div className="Information-details">
